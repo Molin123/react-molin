c640b121e75850f1195eeb4a3a8e7dc7fdc2ba59
shiyanlin <shiyanlin@didichuxing.com>
2017-04-01 15:21:33 +0800

    fetch

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -7,10 +7,30 @@ import { NavBar, Icon } from 'antd-mobile';
 import styles from './../app.less';
 
 export default class Index extends React.Component{
+	testFetch = () => {
+		fetch('/wapapi/User/taskOverView',{
+			method:'post',
+            headers: {
+                "Content-type": "application/x-www-form-urlencoded; charset=UTF-8"
+            },
+            credentials: 'include',
+            body:''
+		})
+		.then((res) => { return res.json(); })
+        .then((data) => {
+            console.log(data)
+        })
+        .catch((e) => { 
+            
+        });
+	}
+	componentDidMount () {
+		this.testFetch()
+	}
 	render(){
         return(
             <div>
-		    	<NavBar leftContent="返回" mode="light" onLeftClick={() => console.log('onLeftClick')}
+		    	<NavBar leftContent="返回1" mode="light" onLeftClick={() => console.log('onLeftClick')}
 		      		rightContent={[
 		        		<Icon key="0" type="search" style={{ marginRight: '0.32rem' }} />,
 		        		<Icon key="1" type="ellipsis" />,
